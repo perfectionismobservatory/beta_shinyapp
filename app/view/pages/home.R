22c41ab6ccaaf6edabc43c799c71761475d36afb
simonsteiger <simon.j.steiger@gmail.com>
2024-02-22 11:45:21 +0200

    chore: update logos and author information in landing page

--- app/view/pages/home.R
+++ app/view/pages/home.R
@@ -22,13 +22,13 @@ ui <- function(id) {
                         sh$div(
                             class = "d-flex justify-content-center align-items-center my-5",
                             sh$img(
-                                src = "static/logo_light_centered.png",
+                                src = "static/logo_light_right.png",
                                 width = "400px",
                                 style = "margin-bottom: 1.5rem;"
                             )
                         ),
                         sh$div(
-                            class = "d-flex flex-row justify-content-center",
+                            class = "d-flex flex-row justify-content-center align-items-center",
                             style = "gap: 4rem;",
                             sh$actionButton(
                                 ns("explore"),
@@ -46,11 +46,51 @@ ui <- function(id) {
                                     sh$p(class = "fs-m", "perfectionism data")
                                 )
                             )
+                        ),
+                        sh$div(
+                            class = "d-flex flex-column justify-content-center align-items-center my-5 gap-3",
+                            sh$img(
+                                src = "static/uni_logos.png",
+                                width = "400px",
+                                style = "text-align: center; margin-top: 2rem;"
+                            ),
+                            sh$div(
+                                sh$h6(
+                                    style = "font-size: 9pt; text-align: center;",
+                                    class = "text-secondary",
+                                    "Thomas Curran¹, Pia Marie Pose¹, Andrew Hill², and Simon Steiger³"
+                                ),
+                                sh$p(
+                                    style = "font-size: 7pt; text-align: center;",
+                                    class = "text-secondary",
+                                    "¹ London School of Economics, ² University of York, ³ Karolinska Institutet"
+                                )
+                            )
                         )
                     ),
                     NULL
                 )
-            )
+            ) # ,
+            # fe$row2(
+            #     colwidths = list(1, 10, 1),
+            #     content = list(
+            #         NULL,
+            #         sh$div(
+            #             class = "d-flex flex-column justify-content-center align-items-center my-5 gap-3",
+            #             sh$img(
+            #                 src = "static/uni_logos.png",
+            #                 width = "400px",
+            #                 style = "text-align: center; margin-top: 1.5rem;"
+            #             ),
+            #             sh$h6(
+            #                 style = "font-size: 9pt; text-align: center;",
+            #                 class = "text-secondary",
+            #                 "Thomas Curran¹, Pia Marie Pose¹, Andrew Hill², and Simon Steiger³"
+            #             )
+            #         ),
+            #         NULL
+            #     )
+            # )
         )
     )
 }
