15a18c34ba6b2a5424f373a42eb0b0d2cd7cfbf6
simonsteiger <simon.j.steiger@gmail.com>
2024-02-22 21:19:59 +0200

    feat: add dynamic updating of scale choices

--- app/view/modules/filter.R
+++ app/view/modules/filter.R
@@ -18,6 +18,31 @@ ui <- function(id) {
         bsl$accordion_panel(
             "Scales",
             icon = bsi$bs_icon("rulers"),
+            fe$radio(
+                ns("view"),
+                "Pick view",
+                choices = c("Higher-order factors" = "hof", "MPS" = "mps")
+            ),
+            fe$radio(
+                ns("new_scale"), # TODO lowest level for each path has to be name of filtered column
+                "Pick dimension",
+                choices = c("Perfectionist strivings" = "str", "Perfectionist concerns" = "con")
+            ),
+            sh$conditionalPanel(
+                condition = "input.view == 'mps'",
+                fe$radio(
+                    ns("new_subscale"),
+                    "Pick subscale",
+                    choices = c(
+                        "Personal standards" = "ps",
+                        "Parental expectation" = "pe",
+                        "Concerns over mistakes" = "com",
+                        "Doubts about actions" = "daa",
+                        "Organisation" = "o"
+                    )
+                ),
+                ns = ns
+            ),
             fe$checkboxgroup(ns("scale"), "Pick scale", choices = c(SOP = "sop", SPP = "spp", OOP = "oop")),
             fe$radio(ns("subscale"), "Pick subscale", choices = c(OM = "om", OSD = "osd"))
         ),
@@ -44,6 +69,51 @@ ui <- function(id) {
 server <- function(id, data) {
     sh$moduleServer(id, function(input, output, session) {
         stopifnot(sh$is.reactive(data))
+
+        # Update scale choices depending on chosen level 1 input
+        sh$observeEvent(input$view, {
+            if (input$view == "hof") {
+                shw$updatePrettyRadioButtons(
+                    session = session,
+                    "new_scale",
+                    choices = c("Perfectionist strivings" = "str", "Perfectionist concerns" = "con")
+                )
+            } else {
+                shw$updatePrettyRadioButtons(
+                    session = session,
+                    "new_scale",
+                    label = "Pick scale",
+                    choices = c("F-MPS" = "fmps", "HF-MPS" = "hfmps")
+                )
+            }
+        })
+
+        sh$observeEvent(input$new_scale, {
+            if (input$new_scale == "fmps") {
+                shw$updatePrettyRadioButtons(
+                    session = session,
+                    "new_subscale",
+                    choices = c(
+                        "Personal standards" = "ps",
+                        "Parental expectation" = "pe",
+                        "Concerns over mistakes" = "com",
+                        "Doubts about actions" = "daa",
+                        "Organisation" = "o"
+                    )
+                )
+            } else {
+                shw$updatePrettyRadioButtons(
+                    session = session,
+                    "new_subscale",
+                    choices = c(
+                        "Self-oriented" = "sop",
+                        "Other-oriented" = "oop",
+                        "Socially-prescribed" = "spp"
+                    )
+                )
+            }
+        })
+
         sieve <- be$filter_inputs(data, input)
         sh$reactive(data()[sieve(), ])
     })
